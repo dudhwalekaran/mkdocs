fd6130b23f107d6ed6581bd35f1a4aee3b2f0178
dudhwalekaran <dudhwalekaran@gmail.com>
2025-02-26 15:06:13 +0530

    Fix pip and mkdocs installation

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+set -e
+
+# Install Python and pip
+export PYTHON_VERSION=3.9
+curl -fsSL https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz | tar xz
+cd Python-$PYTHON_VERSION
+./configure && make && sudo make install
+cd ..
 
 # Install dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
-# Build the MkDocs site
+# Build MkDocs site
 mkdocs build
